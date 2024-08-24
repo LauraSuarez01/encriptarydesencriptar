55a2a7f3e30b56eca64712697645c2f86e4ded86
rrddng <laurasuarez81211@gmail.com>
2024-08-24 16:25:20 -0500

    Cerrando app, actualizada y finalizada

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function encriptar() {
     let texto = document.getElementById("texto").value;
-   let tituloMensaje = document.getElementById("titulo-mensaje");
+    let tituloMensaje = document.getElementById("titulo-mensaje");
     let parrafo = document.getElementById("parrafo");
     let muñeco = document.getElementById("muñeco");
   
@@ -15,11 +15,36 @@ function encriptar() {
       document.getElementById("texto").value = textoCifrado;
       tituloMensaje.textContent = "Texto encriptado con éxito";
       parrafo.textContent = "";
-      muñeco.src = "./img/muñeco.png";
+      muñeco.src = "./img/encriptar.png";
     } else {
-    muñeco.src = "./img/encriptar.png";
+    muñeco.src = "./img/muñeco.png";
     tituloMensaje.textContent = "Ningún mensaje encontrado";
     parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
     alert("Debes ingresar algún texto");
     }
+}
+
+function desencriptar() {
+  let texto = document.getElementById("texto").value;
+  let tituloMensaje = document.getElementById("titulo-mensaje");
+  let parrafo = document.getElementById("parrafo");
+  let muñeco = document.getElementById("muñeco");
+  let textoCifrado = texto
+      .replace(/enter/gi, "e")
+      .replace(/imes/gi, "i")
+      .replace(/ai/gi, "a")
+      .replace(/ober/gi, "o")
+      .replace(/ufat/gi, "u")
+
+      if (texto.length != 0) {
+        document.getElementById("texto").value = textoCifrado;
+      tituloMensaje.textContent = "Texto desencriptado con éxito";
+      parrafo.textContent = "";
+      muñeco.src = "./img/desencriptar.png";
+      } else {
+        muñeco.src = "./img/muñeco.png";
+       tituloMensaje.textContent = "Ningún mensaje encontrado";
+       parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+       alert("Debes ingresar algún texto");
+      }
 }
